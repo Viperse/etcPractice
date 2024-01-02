38ca9d3355db743e9adf35efcdd1a638fe4af0ff
KIM Dokyung <129846485+Viperse@users.noreply.github.com>
2024-01-02 11:57:49 +0900

    refactor: 클라이언트 접속 해제 수정

--- websocket-practice/websocket/src/main/java/com/aroasoft/websocket/config/WebSocketHandler.java
+++ websocket-practice/websocket/src/main/java/com/aroasoft/websocket/config/WebSocketHandler.java
@@ -1,17 +1,13 @@
 package com.aroasoft.websocket.config;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
@@ -41,6 +37,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
             String[] msg = payload.split("/whisper ");
             WebSocketSession wss = map.get(msg[0]);
             wss.sendMessage(new TextMessage(("[귓속말]" + username + " : " + msg[1])));
+
         // 전체 유저에게 전송
         } else {
             for (WebSocketSession wss : map.values()) {
@@ -48,15 +45,14 @@ public class WebSocketHandler extends TextWebSocketHandler {
             }
         }
     }
-    
-    // 월요일에 수정
+
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        for (Map.Entry<String, WebSocketSession> entry : map.entrySet()) {
-            if (entry.getValue().equals(session)) {
-                map.remove(entry.getKey());
-                log.info("{} - 클라이언트 접속 해제", session);
-            }
+        String username = (String) session.getAttributes().get("username");
+
+        if (username != null) {
+            map.remove(username);
+            log.info("{} - 클라이언트 접속 해제", session);
         }
     }
 }
